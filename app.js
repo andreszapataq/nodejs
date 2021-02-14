e11d02a23590e1c55f511d55bc82e483917d19e3
andreszapataq <andreszapataq@gmail.com>
2021-02-14 00:30:30 -0500

    new endpoint

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('HOLA PAPI!');
 });
 
+app.get('/tavo', (req, res) => {
+    res.send('HOLA PAPI 2!');
+});
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
