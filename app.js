f637b07012fc7f000ff06e485a5339d793aba940
andreszapataq <andreszapataq@gmail.com>
2021-02-14 23:14:33 -0500

    add Twilio endpoint

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.get('/tavo', (req, res) => {
     res.send('HOLA PAPI 2!');
 });
 
+app.get('/twilio', (req, res) => {
+    client.messages.create({
+        body: 'HOLA TWILIO FROM AWS!',
+        to: '+573113559747',  // Text this number +12345678901
+        from: '+13347216403' // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+
+    res.send('SMS ENVIADO');
+});
+
 app.listen(process.env.PORT || 5000);
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
@@ -19,12 +30,5 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.t
 
 const twilio = require('twilio');
 const client = new twilio(accountSid, authToken);
-
-client.messages.create({
-    body: 'HOLA PAPITO!',
-    to: '+573113559747',  // Text this number +12345678901
-    from: '+13347216403' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
 
 module.exports = app;
