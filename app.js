2fe7abda30fd1cb7b7a4650a9244b5a0e88fa14a
andreszapataq <andreszapataq@gmail.com>
2021-02-14 23:01:35 -0500

    Twilio auth token solve

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.get('/tavo', (req, res) => {
 
 app.listen(process.env.PORT || 5000);
 
-const accountSid = 'AC693760bad6b60e903146528efad22f04'; // Your Account SID from www.twilio.com/console
-const authToken = '67f86bb1b3efa9a3eb81922dce036d28';   // Your Auth Token from www.twilio.com/console
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
 
 const twilio = require('twilio');
 const client = new twilio(accountSid, authToken);
 
 client.messages.create({
-    body: 'HOLA PAPI!',
+    body: 'HOLA PAPITO!',
     to: '+573113559747',  // Text this number +12345678901
     from: '+13347216403' // From a valid Twilio number
 })
