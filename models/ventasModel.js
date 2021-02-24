52474782d843bcc34ae301e8fc082ad1cbdaa6c4
andreszapataq <andreszapataq@gmail.com>
2021-02-24 18:04:47 -0500

    change ventasModel

--- models/ventasModel.js
+++ models/ventasModel.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const ventaSchema = new mongoose.Schema({
     cliente: String,
     telefono: String,
-    articulo_1: Number,
-    articulo_2: Number,
-    articulo_3: Number
+    articulos: [Object]
 });
 
 const Ventas = mongoose.model('Venta', ventaSchema);
