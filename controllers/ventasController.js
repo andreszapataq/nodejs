50007140af85a8ac10bba5700a9210ad08dfcb2d
andreszapataq <andreszapataq@gmail.com>
2021-02-24 22:12:54 -0500

    get the min and max value

--- controllers/ventasController.js
+++ controllers/ventasController.js
@@ -3,7 +3,14 @@ const Ventas = require('../models/ventasModel');
 exports.getVentas = async (req, res) => {
     const venta = await Ventas.find();
 
-    console.log(venta);
+    const test = venta[0].articulos.map(item => {
+        console.log(item);
+        let arr = Object.values(item);
+        let min = Math.min(...arr);
+        let max = Math.max(...arr);
+
+        console.log(`Min value: ${min}, Max value: ${max}`);
+    });
 
     res.json({
         data: venta
