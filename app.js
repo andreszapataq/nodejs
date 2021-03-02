83133cc7e12dbe363c5a7fde06db11da087bb002
andreszapataq <andreszapataq@gmail.com>
2021-03-01 19:40:33 -0500

    Twilio in ventasController

--- app.js
+++ app.js
@@ -22,32 +22,4 @@ app.get('/tavo', (req, res) => {
 app.use('/ventas', ventasRouter);
 
 
-// TWILIO
-const numbers = process.env.NUMBERS.split(' ');
-
-app.get('/twilio', (req, res) => {
-    Promise.all(
-        numbers.map(number => {
-            return client.messages.create({
-                body: 'TWILIO BULK SMS FROM AWS!',
-                to: number,  // Text this number +12345678901
-                from: '+13347216403' // From a valid Twilio number
-            })
-            .then((message) => console.log(message.sid));
-        })
-    )
-    .then(messages => {
-        console.log('Mensaje enviado!');
-    })
-    .catch(err => console.error(err));
-    
-    res.send('SMS ENVIADO!');
-});
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
-
-const twilio = require('twilio');
-const client = new twilio(accountSid, authToken);
-
 module.exports = app;
--- controllers/ventasController.js
+++ controllers/ventasController.js
@@ -1,17 +1,54 @@
 const Ventas = require('../models/ventasModel');
+const twilio = require('twilio');
 
 exports.getVentas = async (req, res) => {
     const venta = await Ventas.find(); // TRY FINDONE
 
+    console.log(venta);
+
     // const {compras, username, telefono} = venta; TRY THIS
     
     const compras = venta[0].compras;
+
+    const tel = venta[0].telefono;
+
+    console.log(tel);
 
     let min = Math.min(...compras.map(item => item.cantidad));
 
     let menorCompra;
     compras.map(item => item.cantidad === min ? menorCompra = item.articulo : 'NADA!');
     console.log(menorCompra);
+
+
+    // TWILIO
+    const numbers = process.env.NUMBERS.split(' ');
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+    const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+    
+    const client = new twilio(accountSid, authToken);
+
+    const twilio2 = async (req, res) => {
+        await Promise.all(
+            numbers.map(number => {
+                return client.messages.create({
+                    body: `Compra ${menorCompra} este mes con 30% de descuento. Oferta valida solo para ti!`,
+                    to: number,  // Text this number +12345678901
+                    from: '+13347216403' // From a valid Twilio number
+                })
+                .then((message) => console.log(message.sid));
+            })
+        )
+        .then(messages => {
+            console.log('Mensaje enviado!');
+        })
+        .catch(err => console.error(err));
+        
+        // res.send('SMS ENVIADO!');
+    };
+
+    twilio2();
 
     /* let menor;
     let zero = compras[0].cantidad + 1;
